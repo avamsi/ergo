ecedf60f81b11a6bf6b8ad554bb8ecb443fbb532
Vamsi Avula <avamsi07@gmail.com>
2024-08-18 11:40:10 +0530

    errors: add Join to indent underlying errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,6 +1,9 @@
 package errors
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Annotate(err *error, msg string) {
 	if *err != nil {
@@ -11,5 +14,40 @@ func Annotate(err *error, msg string) {
 func Annotatef(err *error, format string, a ...any) {
 	if *err != nil {
 		*err = fmt.Errorf("%s: %w", fmt.Sprintf(format, a...), *err)
+	}
+}
+
+type joinError struct {
+	errs []error
+}
+
+func (err *joinError) Error() string {
+	var b strings.Builder
+	for _, err := range err.errs {
+		b.WriteString("\n\t")
+		b.WriteString(err.Error())
+	}
+	return b.String()
+}
+
+func (err *joinError) Unwrap() []error {
+	return err.errs
+}
+
+func Join(errs ...error) error {
+	var i int
+	for _, err := range errs {
+		if err != nil {
+			errs[i] = err
+			i++
+		}
+	}
+	switch i {
+	case 0:
+		return nil
+	case 1:
+		return errs[0]
+	default:
+		return &joinError{errs[:i:i]}
 	}
 }
--- errors/errors_test.go
+++ errors/errors_test.go
@@ -2,6 +2,7 @@ package errors_test
 
 import (
 	stderrors "errors"
+	"slices"
 	"testing"
 
 	"github.com/avamsi/ergo/errors"
@@ -79,5 +80,73 @@ func TestAnnotatef(t *testing.T) {
 				t.Errorf("Unwrap(%q) = %q, want %q", err, got, test.err)
 			}
 		})
+	}
+}
+
+func TestJoinNil(t *testing.T) {
+	if err := errors.Join(); err != nil {
+		t.Errorf("Join() = %q, want nil", err)
+	}
+	if err := errors.Join(nil); err != nil {
+		t.Errorf("Join(nil) = %q, want nil", err)
+	}
+	if err := errors.Join(nil, nil); err != nil {
+		t.Errorf("errors.Join(nil, nil) = %q, want nil", err)
+	}
+}
+
+func TestJoinUnwrap(t *testing.T) {
+	var (
+		err1  = stderrors.New("err1")
+		err2  = stderrors.New("err2")
+		tests = []struct {
+			errs []error
+			want []error
+		}{
+			{
+				errs: []error{err1, err2},
+				want: []error{err1, err2},
+			},
+			{
+				errs: []error{err1, nil, err2},
+				want: []error{err1, err2},
+			},
+		}
+	)
+	for _, test := range tests {
+		got := errors.Join(test.errs...).(interface{ Unwrap() []error }).Unwrap()
+		if !slices.Equal(got, test.want) {
+			t.Errorf("Join(%q) = %q, want %q", test.errs, got, test.want)
+		}
+	}
+}
+
+func TestJoinError(t *testing.T) {
+	var (
+		err1  = stderrors.New("err1")
+		err2  = stderrors.New("err2")
+		tests = []struct {
+			errs []error
+			want string
+		}{
+			{
+				errs: []error{err1},
+				want: "err1",
+			},
+			{
+				errs: []error{err1, err2},
+				want: "\n\terr1\n\terr2",
+			},
+			{
+				errs: []error{err1, nil, err2},
+				want: "\n\terr1\n\terr2",
+			},
+		}
+	)
+	for _, test := range tests {
+		got := errors.Join(test.errs...).Error()
+		if got != test.want {
+			t.Errorf("Join(%q) = %q, want %q", test.errs, got, test.want)
+		}
 	}
 }
